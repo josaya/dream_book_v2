0806b2567c59455d2c59faa0e301c30f2b781d53
Josh <josaya01@gmail.com>
2023-08-25 12:19:26 +0300

    credentials change

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 from flask import jsonify
 import os
+import urllib
 
 # print(os.getcwd())
 
@@ -14,10 +15,9 @@ for root, dirs, files in os.walk(root_dir):
             certificate_path = os.path.join(root, file)
 
 
-# os.environ['DB_CONNECTION_STRING'] = 'mysql+pymysql://lzgwvmlag20se3ysdz9e:pscale_pw_HgYodV3rimx33cNgf8AjYHUZCHslGPskCcnWdPEFE11@aws.connect.psdb.cloud/dream_book?charset=utf8mb4'
 
-# db_connection_string = "mysql+pymysql://lzgwvmlag20se3ysdz9e:pscale_pw_HgYodV3rimx33cNgf8AjYHUZCHslGPskCcnWdPEFE11@aws.connect.psdb.cloud/dream_book?charset=utf8mb4"
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "mysql+pymysql://i5phka3xjg1vgctbiflg:pscale_pw_m11owQ518QL8JAzwI7LMAcuCsDOb4ufUqlpOYPr4hT4@aws.connect.psdb.cloud/dream_book?charset=utf8mb4"
+
 
 engine = create_engine(db_connection_string,
                        connect_args={
